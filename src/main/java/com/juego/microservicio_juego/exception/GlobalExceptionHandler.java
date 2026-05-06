f920bf19015fc71cb6772d10e40f0512baba4739
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-05-06 19:38:31 -0400

    Manejo de excepciones global

---
+++ src/main/java/com/juego/microservicio_juego/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.juego.microservicio_juego.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> manejoValidaciones (MethodArgumentNotValidException e){
+        HashMap<Object, String> errores = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error-> errores.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errores);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> manejoExcepciones(RuntimeException e){
+        HashMap<Object, Object> error = new HashMap<>();
+
+        error.put("estado",400);
+        error.put("mensaje", e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> manejoGeneral(Exception e){
+        HashMap<Object, Object> error = new HashMap<>();
+
+        error.put("estado",500);
+        error.put("mensaje","Error interno en el servidor");
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+
+
+
+}
